dd361598108c722615a566d7e8a6c4febcd88a4c
Doug Ashton <douglas.j.ashton@gmail.com>
2018-05-08 22:06:11 +0100

    formatting in walking

--- walking_final.R
+++ walking_final.R
@@ -12,33 +12,42 @@ model <- keras_model_sequential()
 
 # Build our CNN
 model %>%
-	  layer_conv_1d(filters = 40, kernel_size = 30, strides = 2,
-			                activation = "relu", input_shape = c(260, 3)) %>%
+  layer_conv_1d(
+    filters = 40,
+    kernel_size = 30,
+    strides = 2,
+    activation = "relu",
+    input_shape = c(260, 3)
+  ) %>%
   layer_max_pooling_1d(pool_size = 2) %>%
-    layer_conv_1d(filters = 40, kernel_size = 10, activation = "relu") %>%
-      layer_max_pooling_1d(pool_size = 2) %>%
-        layer_flatten() %>%
-	  layer_dense(units = 100, activation = "sigmoid") %>%
-	    layer_dense(units = 15, activation = "softmax")
+  layer_conv_1d(filters = 40,
+                kernel_size = 10,
+                activation = "relu") %>%
+  layer_max_pooling_1d(pool_size = 2) %>%
+  layer_flatten() %>%
+  layer_dense(units = 100, activation = "sigmoid") %>%
+  layer_dense(units = 15, activation = "softmax")
 
-    model
+model
 
 
 
-    # Compile
-    model %>%
-	      compile(loss = "categorical_crossentropy",
-		                optimizer = "adam",
-				          metrics = c("accuracy"))
+# Compile
+model %>%
+  compile(loss = "categorical_crossentropy",
+          optimizer = "adam",
+          metrics = c("accuracy"))
 
-    # Run
-    history <- model %>% fit(xWalk$train, yWalk$train,
-			                              epochs = 15, 
-						                               batch_size = 128, 
-						                               validation_split = 0.3,
-									                                verbose = 1)
+# Run
+history <- model %>% fit(
+  xWalk$train,
+  yWalk$train,
+  epochs = 15,
+  batch_size = 128,
+  validation_split = 0.3,
+  verbose = 1
+)
 
-    # Evaluate
-    model %>% 
-	      evaluate(xWalk$test, yWalk$test, verbose = 0)
-
+# Evaluate
+model %>%
+  evaluate(xWalk$test, yWalk$test, verbose = 0)
